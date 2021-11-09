0d65e454b0e166e5d242d5430df44d5a164fc28f
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-11-09 11:32:15 +0100

    Some Triangle

--- Program.cs
+++ Program.cs
@@ -1,19 +1,39 @@
 ﻿using System;
 using GLFW;
+using static OpenGL.Gl;
 
-namespace SharpEngine
-{
-    class Program
-    {
+namespace SharpEngine {
+    class Program {
         static void Main(string[] args) {
             Glfw.Init();
 
             var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
             Glfw.MakeContextCurrent(window);
+            Import(Glfw.GetProcAddress);
 
+            float[] vertices = new float[] {
+                -.5f, -.5f, 0f,
+                .5f, -.5f, 0f,
+                0f, .5f, 0f,
+            };
+
+            var vertexArray = glGenVertexArray();
+            var vertexBuffer = glGenBuffer();
+            glBindVertexArray(vertexArray);
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+            
+            unsafe {
+                fixed (float* vertex = &vertices[0]) {
+                    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.Length, vertex, GL_STATIC_DRAW);
+                }
+                glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), NULL);
+            }
+            glEnableVertexAttribArray(0);
+            
             while (!Glfw.WindowShouldClose(window)) {
-                Glfw.PollEvents(); //react to window changes
-                //do nothing
+                Glfw.PollEvents(); // React to window changes
+                glDrawArrays(GL_TRIANGLES, 0, 3);
+                Glfw.SwapBuffers(window);
             }
         }
     }
