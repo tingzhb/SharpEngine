8f336b79c2e8939d9a2d9885f7eb58553b0d4a16
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-11-15 09:48:09 +0100

    Added Scene, Window, Material and update.

--- Program.cs
+++ Program.cs
@@ -1,109 +1,77 @@
-﻿using System.IO;
-using System.Numerics;
-using System.Runtime.InteropServices;
-using GLFW;
-using static OpenGL.Gl;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SharpEngine
 {
     class Program {
-        
-        private static Triangle triangle = new Triangle(
-            new Vertex[] {
-                new Vertex(new Vector(-.1f, -.1f), Color.Green),
-                new Vertex(new Vector(.1f, -.1f), Color.Blue),
-                new Vertex(new Vector(-.0f, .1f), Color.Red),
-            
-                new Vertex(new Vector(-.4f, -.4f), Color.Red),
-                new Vertex(new Vector(-.2f, -.4f), Color.Green),
-                new Vertex(new Vector(-.3f, -.2f), Color.Blue),
-            }
-       );
+        static float Lerp(float from, float to, float t) {
+            return from + (to - from) * t;
+        }
 
+        static float GetRandomFloat(Random random, float min = 0, float max = 1) {
+            return Lerp(min, max, (float)random.Next() / int.MaxValue);
+        }
+        
+        static void FillSceneWithTriangles(Scene scene, Material material) {
+            var random = new Random();
+            for (var i = 0; i < 10; i++) {
+                var triangle = new Triangle(new Vertex[] {
+                    new Vertex(new Vector(-.1f, 0f), Color.Red),
+                    new Vertex(new Vector(.1f, 0f), Color.Green),
+                    new Vertex(new Vector(0f, .133f), Color.Blue)
+                }, material);
+                triangle.Rotate(GetRandomFloat(random));
+                triangle.Move(new Vector(GetRandomFloat(random, -1, 1), GetRandomFloat(random, -1, 1)));
+                scene.Add(triangle);
+            }
+        }
+        
         static void Main(string[] args) {
             
-            var window = CreateWindow();
-            
-            CreateShaderProgram();
-            
-            var direction = new Vector(0.015f,0.02f);
-            var multiplier = 0.9f;
+            var window = new Window();
+            var material = new Material("shaders/position-color.vert", "shaders/vertex-color.frag");
+            var scene = new Scene();
+            window.Load(scene);
+
+            FillSceneWithTriangles(scene, material);
             
             // engine rendering loop
-            while (!Glfw.WindowShouldClose(window)) {
-                Glfw.PollEvents(); // react to window changes (position etc.)
-                ClearScreen();
-                Render(window);
-                
-                if (triangle.CurrentScale <= 0.2f) {
-                    multiplier = 1.05f;
-                }
-                
-                if (triangle.CurrentScale >= 2f) {
-                    multiplier = 0.95f;
-                }
-                
-                triangle.Scale(multiplier);
+            var direction = new Vector(0.003f, 0.003f);
+            var multiplier = 0.99f;
+            var rotation = 0.05f;
+            while (window.IsOpen()) {
 
-                triangle.Move(direction);
+                // Update Triangles
+                for (var i = 0; i < scene.triangles.Count; i++) {
+                    var triangle = scene.triangles[i];
                 
-
-                if (triangle.GetMaxBounds().x >= 1 && direction.x > 0 || triangle.GetMinBounds().x <= -1 && direction.x < 0) {
-                    direction.x *= -1;
+                    // 2. Keep track of the Scale, so we can reverse it
+                    if (triangle.CurrentScale <= 0.5f) {
+                        multiplier = 1.001f;
+                    }
+                    if (triangle.CurrentScale >= 1f) {
+                        multiplier = 0.999f;
+                    }
+                    
+                    triangle.Scale(multiplier);
+                    triangle.Rotate(rotation);
+                
+                    // 4. Check the X-Bounds of the Screen
+                    if (triangle.GetMaxBounds().x >= 1 && direction.x > 0 || triangle.GetMinBounds().x <= -1 && direction.x < 0) {
+                        direction.x *= -1;
+                    }
+                
+                    // 5. Check the Y-Bounds of the Screen
+                    if (triangle.GetMaxBounds().y >= 1 && direction.y > 0 || triangle.GetMinBounds().y <= -1 && direction.y < 0) {
+                        direction.y *= -1;
+                    }
+                    
+                    
+                    triangle.Move(direction);
                 }
                 
-                if (triangle.GetMaxBounds().y >= 1 && direction.y >= 0 || triangle.GetMinBounds().y <= -1 && direction.y <= 0) {
-                    direction.y *= -1;
-                }
+                window.Render();
             }
-        }
-        
-        private static void Render(Window window) {
-            triangle.Render();
-            Glfw.SwapBuffers(window);
-        }
-        private static void ClearScreen() {
-            glClearColor(.0f, .05f, .2f, 1);
-            glClear(GL_COLOR_BUFFER_BIT);
-        }
-
-        static void CreateShaderProgram() {
-            // create vertex shader
-            var vertexShader = glCreateShader(GL_VERTEX_SHADER);
-            glShaderSource(vertexShader, File.ReadAllText("shaders/position-color.vert"));
-            glCompileShader(vertexShader);
-
-            // create fragment shader
-            var fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-            glShaderSource(fragmentShader, File.ReadAllText("shaders/vertex-color.frag"));
-            glCompileShader(fragmentShader);
-
-            // create shader program - rendering pipeline
-            var program = glCreateProgram();
-            glAttachShader(program, vertexShader);
-            glAttachShader(program, fragmentShader);
-            glLinkProgram(program);
-            glUseProgram(program);
-        }
-
-
-
-        static Window CreateWindow() {
-            // initialize and configure
-            Glfw.Init();
-            Glfw.WindowHint(Hint.ClientApi, ClientApi.OpenGL);
-            Glfw.WindowHint(Hint.ContextVersionMajor, 3);
-            Glfw.WindowHint(Hint.ContextVersionMinor, 3);
-            Glfw.WindowHint(Hint.Decorated, true);
-            Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
-            Glfw.WindowHint(Hint.OpenglForwardCompatible, Constants.True);
-            Glfw.WindowHint(Hint.Doublebuffer, Constants.True);
-
-            // create and launch a window
-            var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
-            Glfw.MakeContextCurrent(window);
-            Import(Glfw.GetProcAddress);
-            return window;
         }
     }
 }
--- Vector.cs
+++ Vector.cs
@@ -9,22 +9,31 @@ namespace SharpEngine {
 			this.y = y;
 			this.z = z;
 		}
+
 		public Vector(float x, float y) {
 			this.x = x;
 			this.y = y;
 			this.z = 0;
 		}
+
 		public static Vector operator *(Vector v, float f) {
 			return new Vector(v.x * f, v.y * f, v.z * f);
 		}
+
+		public static Vector operator /(Vector v, float f) {
+			return new Vector(v.x / f, v.y / f, v.z / f);
+		}
+        
 		public static Vector operator +(Vector lhs, Vector rhs) {
 			return new Vector(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
 		}
+        
 		public static Vector operator -(Vector lhs, Vector rhs) {
 			return new Vector(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
 		}
-		public static Vector operator /(Vector v, float f) {
-			return new Vector(v.x / f, v.y / f, v.z / f);
+        
+		public static Vector operator -(Vector v) {
+			return new Vector(-v.x, -v.y, -v.z);
 		}
         
 		public static Vector Max(Vector a, Vector b) {
@@ -32,7 +41,14 @@ namespace SharpEngine {
 		}
 		public static Vector Min(Vector a, Vector b) {
 			return new Vector(MathF.Min(a.x, b.x), MathF.Min(a.y, b.y), MathF.Min(a.z, b.z));
+		}
 
+		public static float Angle(Vector v) {
+			return MathF.Atan2(v.y, v.x);
+		}
+
+		public float GetMagnitude() {
+			return MathF.Sqrt(x * x + y * y + z * z);
 		}
 	}
 }
